73a589b96ed63c9a0c1069f92d6ac8819777809d
matsuhisa <matsuhisa+github@gmail.com>
2020-04-10 09:06:03 +0900

    Add storybook addon

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
+import { withA11y } from '@storybook/addon-a11y';
 
 export default {
   title: 'Button',
   component: Button,
+  decorators: [withA11y],
 };
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
